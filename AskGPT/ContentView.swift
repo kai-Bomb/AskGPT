2650f2a02f29f5b5ddc216e00a7d07c2c0770108
kai-Bomb <katsumasa.watanabe@zozo.com>
2024-12-05 10:39:07 +0900

    added gitignore

--- AskGPT/ContentView.swift
+++ AskGPT/ContentView.swift
@@ -75,13 +75,12 @@ struct OpenAPIRequester {
     }
 
     func postGPT(_ content: String) async throws -> String {
-        let apiKey = "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"
         var request = URLRequest(
             url: URL(string: "https://api.openai.com/v1/chat/completions")!
         )
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+        request.addValue("Bearer \(Key.apiKey)", forHTTPHeaderField: "Authorization")
         request.httpBody = try JSONEncoder().encode(
             RequestBody(
                 model: "gpt-3.5-turbo",
